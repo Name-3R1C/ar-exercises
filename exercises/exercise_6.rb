3208c96995100d42e7f8ed6b0d00d06fae27b147
Name-3R1C <ericfeng94@hotmail.com>
2023-09-12 22:12:23 -0700

    finish ex 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -17,8 +17,14 @@ class Employee < ActiveRecord::Base
   belongs_to :store
 end
 
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "A", last_name: "E", hourly_rate: 60)
 @store1.employees.create(first_name: "B", last_name: "F", hourly_rate: 60)
 @store2.employees.create(first_name: "C", last_name: "G", hourly_rate: 60)
 @store2.employees.create(first_name: "D", last_name: "H", hourly_rate: 60)
+
+puts @store1.employees.count
+puts @store2.employees.count
