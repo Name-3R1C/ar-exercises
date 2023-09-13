9878b1484879ee7adf9b8626da26e475cef27315
Name-3R1C <ericfeng94@hotmail.com>
2023-09-12 22:39:45 -0700

    finish ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,31 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 6 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {
+    only_integer: true ,
+    greater_than_or_equal_to: 40,
+    less_than_or_equal_to: 200
+  }
+  validates :store, presence: true
+end
+
+puts "Enter a store name"
+name = gets.chomp
+
+store = Store.new(name: name)
+
+if store.save
+  puts "Success create store"
+else
+  puts store.errors.full_messages
+end
